403d3f6ba393b662a0ba4711684ed9a9ee71ab6c
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2022-12-05 09:36:50 +0700

    Add Activity Two (Second Cover Book - GrayScale)

--- app/src/main/java/academy/bangkit/interactivemoduleproject/MainActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/MainActivity.kt
@@ -1,14 +1,27 @@
 package academy.bangkit.interactivemoduleproject
 
+import academy.bangkit.interactivemoduleproject.databinding.ActivityMainBinding
+import academy.bangkit.interactivemoduleproject.databinding.ActivityTwoBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // test
         // test lagi
+
+        binding.btnNext.setOnClickListener{
+            Intent(this@MainActivity, TwoActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
---
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/ThreeActivity.kt
@@ -0,0 +1,11 @@
+package academy.bangkit.interactivemoduleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three)
+    }
+}
