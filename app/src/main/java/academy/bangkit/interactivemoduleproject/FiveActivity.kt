ba6d287908cf854e2ac5ef194cb55d1c5691c088
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2022-12-06 09:53:20 +0700

    Add Activity Four

---
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/FiveActivity.kt
@@ -0,0 +1,11 @@
+package academy.bangkit.interactivemoduleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_five)
+    }
+}
