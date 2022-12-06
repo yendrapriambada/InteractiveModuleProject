b6db00bf6a1423655a3b10cd8a3754e4d675f61c
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2022-12-06 15:38:43 +0700

    Add Activity Seven

--- app/src/main/java/academy/bangkit/interactivemoduleproject/FiveActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/FiveActivity.kt
@@ -1,10 +1,9 @@
 package academy.bangkit.interactivemoduleproject
 
 import academy.bangkit.interactivemoduleproject.databinding.ActivityFiveBinding
-import academy.bangkit.interactivemoduleproject.databinding.ActivityFourBinding
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class FiveActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFiveBinding
@@ -22,6 +21,11 @@ class FiveActivity : AppCompatActivity() {
             }
             btnCp.setOnClickListener {
                 Intent(this@FiveActivity, SixActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            btnTp.setOnClickListener {
+                Intent(this@FiveActivity, SevenActivity::class.java).also {
                     startActivity(it)
                 }
             }
