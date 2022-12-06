2070d1dcdddec9b6d7fcad11485d49635fd4798d
nietzid <arfiansyah@upi.edu>
2022-12-06 23:04:01 +0700

    Update Activity Eleven, Twelve and Thirteen

--- app/src/main/java/academy/bangkit/interactivemoduleproject/ElevenActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/ElevenActivity.kt
@@ -1,20 +1,19 @@
 package academy.bangkit.interactivemoduleproject
 
-import academy.bangkit.interactivemoduleproject.databinding.ActivityMainBinding
-import academy.bangkit.interactivemoduleproject.databinding.ActivityTenBinding
+import academy.bangkit.interactivemoduleproject.databinding.ActivityElevenBinding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TenActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTenBinding
+class ElevenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityElevenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTenBinding.inflate(layoutInflater)
+        binding = ActivityElevenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnPrev.setOnClickListener{
-            Intent(this@TenActivity, ElevenActivity::class.java).also {
+            Intent(this@ElevenActivity, TwelveActivity::class.java).also {
                 startActivity(it)
             }
         }
---
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/TwelveActivity.kt
@@ -0,0 +1,22 @@
+package academy.bangkit.interactivemoduleproject
+
+import academy.bangkit.interactivemoduleproject.databinding.ActivityElevenBinding
+import academy.bangkit.interactivemoduleproject.databinding.ActivityTwelveBinding
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwelveActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTwelveBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTwelveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnPrev.setOnClickListener{
+            Intent(this@TwelveActivity, ThirteenActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+}
