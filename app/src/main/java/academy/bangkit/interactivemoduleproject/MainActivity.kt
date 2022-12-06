ce3f5e37065a8bd37cb7aa3f65907d71bec34306
nietzid <arfiansyah@upi.edu>
2022-12-06 17:31:28 +0700

    Update Activity One

--- app/src/main/java/academy/bangkit/interactivemoduleproject/MainActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/MainActivity.kt
@@ -1,14 +1,25 @@
 package academy.bangkit.interactivemoduleproject
 
+import academy.bangkit.interactivemoduleproject.databinding.ActivityMainBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // test
         // test lagi
+
+        binding.btnNext.setOnClickListener{
+            Intent(this@MainActivity, TenActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
