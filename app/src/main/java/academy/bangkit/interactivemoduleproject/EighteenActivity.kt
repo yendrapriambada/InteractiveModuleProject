0274c4895cf74bb07ef61621add351d7a8ef327e
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2022-12-10 14:06:35 +0700

    part 3 Finishing: fix bug data & layout

--- app/src/main/java/academy/bangkit/interactivemoduleproject/EighteenActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/EighteenActivity.kt
@@ -3,6 +3,8 @@ package academy.bangkit.interactivemoduleproject
 import academy.bangkit.interactivemoduleproject.databinding.ActivityEighteenBinding
 import academy.bangkit.interactivemoduleproject.databinding.ActivitySevenTeenBinding
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -14,7 +16,20 @@ class EighteenActivity : AppCompatActivity() {
         binding = ActivityEighteenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sharedPref = applicationContext.getSharedPreferences("MyPref", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        binding.edtRefleksi321.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+
+        val dataRefleksi321 = sharedPref.getString("refleksi321","default").toString()
+
+        if (dataRefleksi321!= "default"){
+            binding.edtRefleksi321.setText(dataRefleksi321)
+        }
+
         binding.btnMenu.setOnClickListener {
+            editor.putString("refleksi321", binding.edtRefleksi321.text.toString().trim())
+            editor.apply()
             Intent(this, FiveActivity::class.java).also {
                 startActivity(it)
                 finish()
--- app/src/main/java/academy/bangkit/interactivemoduleproject/TwentyThreeActivity.kt
+++ app/src/main/java/academy/bangkit/interactivemoduleproject/TwentyThreeActivity.kt
@@ -1,11 +1,23 @@
 package academy.bangkit.interactivemoduleproject
 
-import androidx.appcompat.app.AppCompatActivity
+import academy.bangkit.interactivemoduleproject.databinding.ActivityTwentyThreeBinding
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class TwentyThreeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTwentyThreeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_twenty_three)
+        binding = ActivityTwentyThreeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPrev.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
     }
 }
